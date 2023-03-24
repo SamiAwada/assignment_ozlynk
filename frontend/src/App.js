d0cd9726ab676fb8a92f0291cba2c0ddb8b06bb6
Metariba <infometariba@gmail.com>
2023-03-24 02:24:03 +0200

    Added mainlayout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages";
+import { MainLayout } from "./layouts";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" index element={<LandingPage />} />
-        <Route
-          exact
-          path="/addEmployee"
-          index
-          element={<div>addEmployee</div>}
-        />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route exact path="/" index element={<LandingPage />} />
+          <Route
+            exact
+            path="/Employee"
+            index
+            element={<div>addEmployee</div>}
+          />
+        </Routes>
+      </MainLayout>
     </div>
   );
 }
---
+++ frontend/src/layouts/index.js
@@ -0,0 +1,3 @@
+import MainLayout from "./mainLayout/index";
+
+export { MainLayout };
