1200ab7d72266c928bfe98562120566f811bd4bb
Metariba <infometariba@gmail.com>
2023-03-26 22:18:39 +0300

    added body-parser and route employee

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
 var express = require("express");
 var cors = require("cors");
-
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 4000;
+const employeeRouter = require("./src/routes/employee");
 
+app.use("/employee", employeeRouter);
 app.get("/", (req, res) => {
   res.send({ msg: "React Client connected to the Express server!" });
 });
 
-
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App Listening at http://localhost:${port}`);
 });
