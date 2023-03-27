9509e28fa3f86c16138ad8d98674aae31050a471
Metariba <infometariba@gmail.com>
2023-03-27 06:48:26 +0300

    added employee getall and getbyname requestes

--- backend/src/routes/employee.js
+++ backend/src/routes/employee.js
@@ -1,6 +1,41 @@
 const router = require("express").Router();
 const dbConnection = require("../db/dbConnect");
 
+router.get("/getAll", async (req, res) => {
+  try {
+    let call;
+    call = await dbConnection.connect();
+    const query = `SELECT * FROM public.employees;`;
+    call = await dbConnection.query(query);
+
+    // await dbConnection.release();
+    res.status(200).json({ msg: call });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: error });
+  }
+});
+
+router.get("/getByName/:search", async (req, res) => {
+  try {
+    let call;
+    let { search } = req.params;
+    call = await dbConnection.connect();
+    console.log(search);
+    const query = `SELECT * FROM public.employees WHERE "name" LIKE '%${search}%' `;
+
+    call = await dbConnection.query(query);
+
+    console.log(call.rows);
+    // await dbConnection.end();
+    console.log(call);
+    res.status(200).json({ msg: call.rows });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: error });
+  }
+});
+
 router.post("/add", async (req, res) => {
   try {
     let call;
@@ -19,5 +54,6 @@ router.post("/add", async (req, res) => {
     res.status(404).json({ msg: "Somthing went wrong!!!" });
   }
 });
+
 
 module.exports = router;
