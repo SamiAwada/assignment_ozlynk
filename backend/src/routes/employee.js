a89caa9ed086b1d53e42e9ac458ea622741da62a
Metariba <infometariba@gmail.com>
2023-03-27 06:49:51 +0300

    added update and delete requests

--- backend/src/routes/employee.js
+++ backend/src/routes/employee.js
@@ -55,5 +55,50 @@ router.post("/add", async (req, res) => {
   }
 });
 
+router.put("/update", async (req, res) => {
+  try {
+    let call;
+    const { id, name, email, phoneNumber, salary } = req.body;
+    console.log(req.body);
+
+    call = await dbConnection.connect();
+
+    const text = `UPDATE public.employees SET name = '${name}', email = '${email}', phonenumber = '${phoneNumber}', salary = '${salary}' WHERE "id" = ${id}`;
+    // console.log("TEXT : ", text);
+
+    call = await dbConnection.query(text);
+
+    console.log("RES : ", call);
+    res.status(200).json({ msg: "Employee Updated Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: "Somthing went wrong!!!" });
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  try {
+    let call;
+    const { id } = req.body;
+    console.log(req.body);
+
+    call = await dbConnection.connect();
+
+    const text = `DELETE FROM public.employees WHERE id = $1`;
+    // console.log("TEXT : ", text);
+
+    call = await dbConnection.query(text, [id]);
+
+    console.log("RES : ", call);
+    res
+      .status(200)
+      .json({ msg: `Employee with id: ${id} Deleted Successfully` });
+
+    // await dbConnection.end();
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ msg: "Somthing went wrong!!!" });
+  }
+});
 
 module.exports = router;
